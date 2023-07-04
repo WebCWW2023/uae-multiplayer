602bc5101974dcccb2bd17bc28799e415e11b3d0
Web2023 <86647112+WebCWW2023@users.noreply.github.com>
2023-07-04 14:30:54 +0530

    Update voice.js

--- game/voice/voice.js
+++ game/voice/voice.js
@@ -1,5 +1,5 @@
 import { isadmin } from "../game.js";
-
+ 
 const socket = io();
 
 let options = {
@@ -25,64 +25,47 @@ var remoteUser = {};
 
 async function startBasicCall() {
   const agoraEngine = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
-
   agoraEngine.on("user-published", async (user, mediaType) => {
-    await agoraEngine.subscribe(user, mediaType);
-    console.log("mediaType", mediaType);
+    await agoraEngine.subscribe(user, mediaType);z
     if (mediaType == "audio") {
       const remoteUid = user.uid;
-
       // Get the RemoteAudioTrack object from the AgoraRTCRemoteUser object.
       const remoteAudioTrack = user.audioTrack;
       remoteUser[remoteUid] = remoteAudioTrack;
       remoteAudioTrack.play();
     }
-
     // Listen for the "user-unpublished" event.
-    agoraEngine.on("user-unpublished", (user) => {});
+    agoraEngine.on("user-unpublished", (user) => { });
   });
-}
 
-function startVoice() {
-  let search_params = new URL(window.location.href).searchParams;
-  var avtarName = search_params.get("name");
-  var roomName = search_params.get("room");
-  // Join a channel.
-  options.channel = roomName;
-  socket.emit("checkAgoraToken", roomName);
+  startVoice = () => {
+    let search_params = new URL(window.location.href).searchParams;
+    var avtarName = search_params.get("name");
+    var roomName = search_params.get("room");
+    // Join a channel.
+    options.channel = roomName;
+     socket.emit("checkAgoraToken", roomName);
+    socket.on("getTokenFromDB", async (token) => {
+      options.token = token;
+      await agoraEngine.join(
+        options.appId,
+        options.channel,
+        options.token,
+        options.uid
+      );
+      /*------------------------------------*/
 
-  socket.on("getTokenFromDB", async (token) => {
-    options.token = token;
+      socket.emit("addVoiceId", {
+        voiceId: agoraEngine.uid,
+        avtarName: avtarName,
+        roomName: roomName,
+      });
+      channelParameters.localAudioTrack =
+        await AgoraRTC.createMicrophoneAudioTrack();
+      await agoraEngine.publish(channelParameters.localAudioTrack);
 
-    await agoraEngine.join(
-      options.appId,
-      options.channel,
-      options.token,
-      options.uid
-    );
-    /*------------------------------------*/
-
-    socket.emit("addVoiceId", {
-      voiceId: agoraEngine.uid,
-      avtarName: avtarName,
-      roomName: roomName,
-    });
-    channelParameters.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
-    await agoraEngine.publish(channelParameters.localAudioTrack);
-
-    muteButton = document.querySelector(".selfMute");
-    muteButton.addEventListener("click", async function () {
-      if (!channelParameters.localAudioTrack.enabled) {
-        await channelParameters.localAudioTrack.setEnabled(true);
-        muteButton.innerHTML = '<i class="fas fa-microphone"></i>';
-      } else {
-        await channelParameters.localAudioTrack.setEnabled(false);
-        muteButton.innerHTML = '<i class="fas fa-microphone-slash"></i>';
-      }
-    });
-
-    document.addEventListener("keyup", async function (e) {
-      if (e.code === "KeyM") {
+      muteButton = document.querySelector(".selfMute");
+      muteButton.addEventListener("click", async function () {
         if (!channelParameters.localAudioTrack.enabled) {
           await channelParameters.localAudioTrack.setEnabled(true);
           muteButton.innerHTML = '<i class="fas fa-microphone"></i>';
@@ -90,9 +73,28 @@ function startVoice() {
           await channelParameters.localAudioTrack.setEnabled(false);
           muteButton.innerHTML = '<i class="fas fa-microphone-slash"></i>';
         }
-      }
-    });
-  });
-}
+      })
+      document.addEventListener("keyup", async function (e) {
+        if (e.code === "KeyM") {
+          if (!channelParameters.localAudioTrack.enabled) {
+            await channelParameters.localAudioTrack.setEnabled(true);
+            muteButton.innerHTML = '<i class="fas fa-microphone"></i>';
+          } else {
+            await channelParameters.localAudioTrack.setEnabled(false);
+            muteButton.innerHTML = '<i class="fas fa-microphone-slash"></i>';
+          }
+        }
+      })
 
+    });
+  };
+  agoraEngine.enableAudioVolumeIndicator();
+  // agoraEngine.on("volume-indicator", (volumes) => {
+  //   volumes.forEach((volume) => {
+  //     if (volume.level > 0) {
+  //     }
+  //   });
+  // });
+  // console.clear()
+}
 export { startBasicCall, remoteUser, startVoice };
